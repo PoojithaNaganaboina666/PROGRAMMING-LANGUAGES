f0f9993239fd2c170109ec86c6a08f2905b8ad9c
PoojithaNaganaboina666 <poojithanaganaboina666@gmail.com>
2025-08-28 22:25:03 +0530

    Add files via upload

---
+++ t1.py
@@ -0,0 +1,8 @@
+n=int(input())
+for row in range(n):
+    for column in range(n):
+        if(row==0 or column==0 or row ==n-1 or column== n-1):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
