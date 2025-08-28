9b764c8ccd29a23ff23c49b3d08608aa24020364
PoojithaNaganaboina666 <poojithanaganaboina666@gmail.com>
2025-08-28 22:32:25 +0530

    Add files via upload

--- t3.py
+++ t3.py
@@ -1,8 +1,8 @@
 n=int(input())
 for row in range(n):
-    for col in range(row):
+    for col in range(n-row-1):
         print(" ",end=" ")
-    for col in range(2*(n-row)-1):
+    for col in range(2*row+1):
         print("*",end=" ")
     print()
 
